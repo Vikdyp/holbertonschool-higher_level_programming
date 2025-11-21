c93a0df2e9c001e30e58cd763327f63607c9a5cb
Vikdyp <victor@roubin.net>
2025-11-21 14:45:13 +0000

    correction format

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -4,5 +4,5 @@ def uppercase(str):
         ascii_code = ord(c)
         if 97 <= ascii_code <= 122:
             ascii_code = ascii_code - 32
-        print(chr(ascii_code), end="")
+        print("{}".format(chr(ascii_code)), end="")
     print()
