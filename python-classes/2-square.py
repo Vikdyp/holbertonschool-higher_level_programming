29a09eb0714b6cb8141e455041723fa5d3e4d93a
Vikdyp <victor@roubin.net>
2025-11-24 19:05:28 +0100

    Size validation

---
+++ python-classes/2-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+class Square:
+    """class qui defini un carre"""
+
+    def __init__(self, size=0):
+        """initialise un carre avec une taille optionnel"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
