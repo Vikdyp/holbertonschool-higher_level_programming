f4e58d1d03acf8d0a40d69a14bafd90e830b389e
Vikdyp <victor@roubin.net>
2025-11-24 19:18:00 +0100

    Area of a square

---
+++ python-classes/3-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""module qui definit la classe Square"""
+
+
+class Square:
+    """class qui defini un carre"""
+
+    def __init__(self, size=0):
+        """initialise un carre avec une taille optionnel"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """fonction qui retourne l'aire du carre"""
+        return self.__size * self.__size
