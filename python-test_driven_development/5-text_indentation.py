2ee4a092af53ef2183a8674a1c09b2822bf35235
Vikdyp <victor@roubin.net>
2025-11-25 17:57:16 +0100

    Text indentation

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Module that provide a function that print a text"""
+
+
+def text_indentation(text):
+    """function that prints a text with 2 new lines after each of these
+     characters: . ? and :"""
+
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    separators = ".?:"
+    result = ""
+
+    for i in text:
+        result += i
+        if i in separators:
+            print(result.strip())
+            print()
+            result = ""
+    if result.strip():
+        print(result.strip())
