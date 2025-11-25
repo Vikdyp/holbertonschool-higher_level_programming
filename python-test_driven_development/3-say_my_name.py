1f6f374482fd5de684a9022fba2ee79bbb6fb172
Vikdyp <victor@roubin.net>
2025-11-25 16:53:54 +0100

    Say my name

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Module that provide a function to print first and last name"""
+
+
+def say_my_name(first_name, last_name=""):
+    """function that print first and last name"""
+
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+
+    print(f"My name is {first_name} {last_name}")
