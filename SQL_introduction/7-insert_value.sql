5d8416cedd10b621cfbf2fd472477505d7a221d6
Vikdyp <victor@roubin.net>
2025-11-28 12:08:01 +0000

    correction syntax

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,4 +1,4 @@
 -- Task: Ajoute des donnes dans la table first_table
 
 -- Ajoute des donnes dans la table
-INSERT INTO first_table (id, name) VALUES (89, Best School)
+INSERT INTO first_table (id, name) VALUES (89, 'Best School')
