b3be68735e9ba27ba186a81de5c0b8cf3da2d66b
Vikdyp <victor@roubin.net>
2025-11-28 12:20:27 +0000

    Full creation

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,12 @@
+-- Task: Cree une table second_table
+
+-- Cree la table
+CREATE TABLE IF NOT EXISTE second_table(
+	id INT,
+	name VARCHAR(256),
+	score INT
+);
+INSERT INTO second_table (id, name, score) VALUES (1, 'John', 10);
+INSERT INTO second_table (id, name, score) VALUES (2, 'Alex', 3);
+INSERT INTO second_table (id, name, score) VALUES (3, 'Bob', 14);
+INSERT INTO second_table (id, name, score) VALUES (4, 'George', 8);
