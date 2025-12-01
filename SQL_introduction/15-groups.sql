acd67371d546ddbdbb2a4a551264f49c1e530218
vikdyp <victor@roubin.net>
2025-12-01 09:06:07 +0000

    Number by score

---
+++ SQL_introduction/15-groups.sql
@@ -0,0 +1,7 @@
+-- Task: Affiche combien de fois chaque score est present
+
+-- Compte le nombre de fois qu'un score est present
+SELECT score COUNT(*) AS number
+FROM second_table
+GROUP BY score
+ORDER BY number DESC
