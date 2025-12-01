602ba931f176250811fe456faaff166d63a4daa1
vikdyp <victor@roubin.net>
2025-12-01 08:14:38 +0000

    Select the best

---
+++ SQL_introduction/11-best_score.sql
@@ -0,0 +1,5 @@
+# Task: Liste toute les entrer de la table second_table avec un score >= 10
+
+# Affiche le score et le nom dans order by score:x
+SELECT score, name FROM second_table WHERE score >= 10
+ORDER BY score DESC;
