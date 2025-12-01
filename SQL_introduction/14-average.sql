48926503604a648620dd5119ca9f0602e9ba1e27
vikdyp <victor@roubin.net>
2025-12-01 09:00:09 +0000

    correction uppercase

--- SQL_introduction/14-average.sql
+++ SQL_introduction/14-average.sql
@@ -1,4 +1,4 @@
 -- Task: Affiche le score moyen de toutes les entrer de la table second_table
 
 -- Affiche le score moyen
-SELECT AVG(score) AS average from second_table;
+SELECT AVG(score) AS average FROM second_table;
