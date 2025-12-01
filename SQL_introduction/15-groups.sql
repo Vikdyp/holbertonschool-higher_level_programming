223fd34fe63e3c3f7864606d574985ffeb6c3e46
vikdyp <victor@roubin.net>
2025-12-01 09:08:04 +0000

    correction oublie virgule

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,7 +1,7 @@
 -- Task: Affiche combien de fois chaque score est present
 
 -- Compte le nombre de fois qu'un score est present
-SELECT score COUNT(*) AS number
+SELECT score, COUNT(*) AS number
 FROM second_table
 GROUP BY score
-ORDER BY number DESC
+ORDER BY number DESC;
